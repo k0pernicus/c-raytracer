809841de504823093d090b37be15da4d68dbec45
k0pernicus <>
2020-07-27 16:12:29 +0200

    User can enter the filename as first program parameter

--- raytracer.c
+++ raytracer.c
@@ -2,8 +2,14 @@
 #include "ppm.h"
 #include "vec3d.h"
 
-int main(void)
+const char* DEFAULT_OUTPUT_NAME = "output.ppm";
+
+int main(int argc, char* argv[])
 {
+    char* ppm_filename = DEFAULT_OUTPUT_NAME;
+    if (argc >= 2) {
+        ppm_filename = argv[1];
+    }
     // ppm_buffer buffer = generate_fake_data();
     const vec3d camera = {
         0.0,
@@ -11,7 +17,7 @@ int main(void)
         0.0,
     };
     ppm_buffer buffer = generate_gradient_data_from(&camera);
-    ppm_to_file(buffer, "output.ppm");
+    ppm_to_file(buffer, ppm_filename);
     free(buffer);
     return 0;
 }
